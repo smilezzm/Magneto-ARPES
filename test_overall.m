6fc1378bbfa47a7e926c059dc748b50bbf50d8f0
mengyao <mengyao@stu.pku.edu.cn>
2025-09-17 19:58:53 -0500

    build an overall flow

--- test_overall.m
+++ test_overall.m
@@ -3,8 +3,8 @@ k_r = 0.4;
 final_measured = load('fermi_surface_30mA.mat','I_thetax_thetay','kx','ky');
 standard_measured = load('fermi_surface_0mA.mat','I_thetax_thetay','kx','ky');
 initial_guess = [0;0;0;0;0;0;0.025];
-calc_standard_field()
 tic
+standard_field = calc_standard_field(0.016,true);
 parameters = optimized_parameters(standard_measured, final_measured, k_r, standard_field, initial_guess);
+[Fkx,Fky] = calc_inverse_mapping(standard_field, parameters);
 elapsed_time = toc;
-save('optimized_p.mat','parameters','elapsed_time');
