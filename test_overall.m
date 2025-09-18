cc3baa2187eb618fdba246c8b5999987819ac97d
mengyao <mengyao@stu.pku.edu.cn>
2025-09-17 19:57:10 -0500

    overall flow

--- test_overall.m
+++ test_overall.m
@@ -1,9 +1,9 @@
 clear; 
 k_r = 0.4;
-final_measured = load('fermi_surface.mat','I_thetax_thetay','kx','ky');
+final_measured = load('fermi_surface_30mA.mat','I_thetax_thetay','kx','ky');
 standard_measured = load('fermi_surface_0mA.mat','I_thetax_thetay','kx','ky');
-standard_field = load('standard_field.mat','X','Y','Z','Bx','By','Bz');
 initial_guess = [0;0;0;0;0;0;0.025];
+calc_standard_field()
 tic
 parameters = optimized_parameters(standard_measured, final_measured, k_r, standard_field, initial_guess);
 elapsed_time = toc;
